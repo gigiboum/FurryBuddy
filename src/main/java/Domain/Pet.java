d4f62478b914a34e3600393df619726027fac349
jeremy <jeremy.furlan@unil.ch>
2024-10-28 11:31:51 +0100

    add class pet

--- src/main/java/Domain/Pet.java
+++ src/main/java/Domain/Pet.java
@@ -1,4 +1,27 @@
 package Domain;
 
 public class Pet {
+    private String petId;
+    private String name;
+    private String species;
+    private String race;
+    private boolean neutered;
+    private String gender;
+    private String description;
+    private String personality;
+    private String color;
+    private boolean isCompatibleWithInexperiencedOwners;
+    private boolean isCompatibleWithKids;
+    private boolean isCompatibleWithFamilies;
+    private boolean isCompatibleWithOtherAnimals;
+    private int age;
+    private double price;
+    private Status status;
+    private boolean isSuitableForHouse;
+    private boolean isVaccinated;
+    private String medicalConditions;
+
+    public enum Status {
+        AVAILABLE, PENDING, ADOPTED
+    }
 }
