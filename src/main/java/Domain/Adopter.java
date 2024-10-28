e514757800dc97e7f2fafad447132765f1deb97c
mila <mila.livron@unil.ch>
2024-10-28 12:51:51 +0100

    add adopter details

--- src/main/java/Domain/Adopter.java
+++ src/main/java/Domain/Adopter.java
@@ -1,4 +1,31 @@
 package Domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Adopter {
+
+    private String name;
+    private String email;
+    private List<String> filters;
+    private List<Pet> availablePets;
+    private List<AdoptionRequest> adoptionRequests;
+
+    public Adopter(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.filters = new ArrayList<>();
+        this.availablePets = new ArrayList<>();
+        this.adoptionRequests = new ArrayList<>();
+    }
 }
+
+    // Permet de trouver un animal avec ou sans filtres
+   /* public List<Pet> findPet(List<String> filters) {
+        List<Pet> filteredPets = new ArrayList<>();
+
+        if (filters == null || filters.isEmpty()) {
+            return availablePets; // Retourne tous les animaux disponibles si aucun filtre
+        }
+
+    }*/
