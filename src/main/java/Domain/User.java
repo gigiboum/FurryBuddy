04f66e0a8d6d4ed386822a8db376313b04ad452b
jeremy <jeremy.furlan@unil.ch>
2024-10-29 11:54:27 +0100

    add class pet

--- src/main/java/Domain/User.java
+++ src/main/java/Domain/User.java
@@ -1,6 +1,22 @@
 package Domain;
 
-public class User {
-    private String name;
+public abstract class User {
+    private String userId;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private location location;
+    private role role;
 
-}
+    public User(String userId, String password, String firstName, String lastName, Location location, Role role) {
+        this.userId = userId;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.location = location;
+        this.role = role;
+    }
+
+    public enum Role {
+        ADOPTER, PET_OWNER
+    }
