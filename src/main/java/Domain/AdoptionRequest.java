c357fe18cc1f8464b8a6f4d4e3dff9329fb73f7e
chiarabrunschwig <chiara.brunschwig@unil.ch>
2024-11-01 15:44:31 +0100

    getters and setters AR

--- src/main/java/Domain/AdoptionRequest.java
+++ src/main/java/Domain/AdoptionRequest.java
@@ -1,5 +1,7 @@
 package Domain;
 
+import java.util.UUID;
+
 public class AdoptionRequest {
     private String requestID;
     private Adopter adopter;
@@ -12,7 +14,55 @@ public class AdoptionRequest {
     }
 
     // Constructors
-    public AdoptionRequest(String requestID, Adopter adopter, Advertisement advertisement, Status status) {}
+    public AdoptionRequest(Adopter adopter, Advertisement advertisement) {
+        this.requestID = UUID.randomUUID().toString();
+        this.adopter = adopter;
+        this.advertisement = advertisement;
+        this.status = Status.PENDING; // Statut initial de la demande (not sure)
+    }
 
-    //Getters and Setters
+    public AdoptionRequest(String requestID, Adopter adopter, Advertisement advertisement, Status status) {
+        this.requestID = requestID;
+        this.adopter = adopter;
+        this.advertisement = advertisement;
+        this.status = status;
+    }
+
+// Getters and Setters
+    public String getRequestID() {
+        return requestID;
+    }
+
+    public void setRequestID(String requestID) {
+        this.requestID = requestID;
+    }
+
+    public Adopter getAdopter() {return adopter;
+    }
+
+    public void setAdopter(Adopter adopter) {this.adopter = adopter;
+    }
+
+    public Advertisement getAdvertisement() {return advertisement;
+    }
+
+    public void setAdvertisement(Advertisement advertisement) {this.advertisement = advertisement;
+    }
+
+    public Status getStatus() {return status;
+    }
+
+    public void setStatus(Status status) {this.status = status;
+    }
+
+    public void approveRequest() {this.status = Status.APPROVED;
+    }
+    public void rejectRequest() {this.status = Status.REJECTED;
+    }
+    public boolean isPending() {return this.status == Status.PENDING;
+    }
+
 }
+
+
+
