f5e2fd5b7e380fa807989db5c04e6ad6f10b5cc6
audreymartelly <73237246+audreymartelly@users.noreply.github.com>
2024-11-03 20:30:58 +0100

    new objects (tbd)

---
+++ src/main/java/Domain/ExceptionDescription.java
@@ -0,0 +1,29 @@
+package Domain;
+
+public class ExceptionDescription {
+    private String type;
+    private String message;
+
+    public ExceptionDescription() {}
+
+    public ExceptionDescription(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/Domain/Location.java
@@ -0,0 +1,35 @@
+package Domain;
+
+public class Location {
+    private String town;
+    private String postalCode;
+    private String address;
+
+    public Location(String town, String postalCode, String address) {
+        this.town = town;
+        this.postalCode = postalCode;
+        this.address = address;
+    }
+
+    // getters and setters
+
+    public String getTown() {
+        return town;
+    }
+    public void setTown(String town) {
+        this.town = town;
+    }
+    public String getPostalCode() {
+        return postalCode;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
